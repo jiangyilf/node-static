72ad4570450f0e9d2e4b6fc2999118ffcc8c11f5
jiangyi <jiangyi@cdsb.com>
2019-03-20 15:14:54 +0800

    index

--- app-static.js
+++ app-static.js
@@ -2,17 +2,24 @@ const Hapi = require('hapi')
 const Path = require('path')
 
 const static = require('./routes/static')
+const home = require('./routes/home')
 
 const server = Hapi.server({
     port: 3000,
     host: '127.0.0.1',
+    routes: {
+        files: {
+            relativeTo: Path.join(__dirname, 'public')
+        }
+    }
 })
 
 const init = async () => {
     await server.register(require('inert'))
 
     server.route([
-        ...static
+        ...static,
+        ...home,
     ])
 
     await server.start()
